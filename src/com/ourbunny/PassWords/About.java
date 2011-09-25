76cb1e4fc44e7e855c23960691fdfdbebdb15fd6
Tommy Murphy <tommy@ourbunny.com>
2011-09-25 09:36:42 -0700

    Initial 1.0

---
+++ src/com/ourbunny/PassWords/About.java
@@ -0,0 +1,49 @@
+package com.ourbunny.PassWords;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+public class About extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+        
+        PackageInfo packageInfo;
+        String strVersion;
+		try {
+			packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			strVersion = packageInfo.versionName;
+        } catch (NameNotFoundException e) {
+        	e.printStackTrace();
+        	strVersion = "Undefined Version";
+        }
+        
+        // text2 has links specified by putting <a> tags in the string
+        // resource.  By default these links will appear but not
+        // respond to user input.  To make them active, you need to
+        // call setMovementMethod() on the TextView object.
+        
+        TextView t_version = (TextView) findViewById(R.id.textveiw_version);
+        t_version.setText(strVersion);
+        
+        TextView t_contact = (TextView) findViewById(R.id.textview_contact);
+        t_contact.setMovementMethod(LinkMovementMethod.getInstance());
+        
+        findViewById(R.id.button_back).setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// Log.d(TAG, "Generate PRESSED!!!!!");
+				Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+			}
+		});
+    }
+}
---
+++ src/com/ourbunny/PassWords/Generate.java
@@ -0,0 +1,141 @@
+package com.ourbunny.PassWords;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;;
+
+
+public class Generate extends Activity {
+	private List<String> wordList;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        this.readWordlist();
+        
+        /** Callback for generate button */
+        findViewById(R.id.generate).setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				String words[] = new String[4];
+				
+				((ToggleButton)findViewById(R.id.toggleButton1)).setEnabled(true);
+				((ToggleButton)findViewById(R.id.toggleButton2)).setEnabled(true);
+				((ToggleButton)findViewById(R.id.toggleButton3)).setEnabled(true);
+				((ToggleButton)findViewById(R.id.toggleButton4)).setEnabled(true);
+				
+				// Log.d(TAG, "Generate PRESSED!!!!!");
+				if ( ((ToggleButton)findViewById(R.id.toggleButton1)).isChecked() == false) {
+					((TextView)findViewById(R.id.textView1)).setText(getWord());
+				}
+				if ( ((ToggleButton)findViewById(R.id.toggleButton2)).isChecked() == false) {
+					((TextView)findViewById(R.id.textView2)).setText(getWord());
+				}
+				if ( ((ToggleButton)findViewById(R.id.toggleButton3)).isChecked() == false) {
+					((TextView)findViewById(R.id.textView3)).setText(getWord());
+				}
+				if ( ((ToggleButton)findViewById(R.id.toggleButton4)).isChecked() == false) {
+					((TextView)findViewById(R.id.textView4)).setText(getWord());
+				}
+				words[0]= (String) ((TextView)findViewById(R.id.textView1)).getText();
+				words[1]= (String) ((TextView)findViewById(R.id.textView2)).getText();
+				words[2]= (String) ((TextView)findViewById(R.id.textView3)).getText();
+				words[3]= (String) ((TextView)findViewById(R.id.textView4)).getText();
+				((EditText)findViewById(R.id.textPass)).setText(words[0] + ' ' + words[1] + ' ' + words[2] + ' ' + words[3]);
+			}
+		});
+        
+        /** Callback for generate button */
+        findViewById(R.id.clear).setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// Log.d(TAG, "Generate PRESSED!!!!!");
+				((ToggleButton)findViewById(R.id.toggleButton1)).setChecked(false);
+				((ToggleButton)findViewById(R.id.toggleButton2)).setChecked(false);
+				((ToggleButton)findViewById(R.id.toggleButton3)).setChecked(false);
+				((ToggleButton)findViewById(R.id.toggleButton4)).setChecked(false);
+				
+				((EditText)findViewById(R.id.textPass)).setText("");
+				
+				((TextView)findViewById(R.id.textView1)).setText(R.string.empty);
+				((TextView)findViewById(R.id.textView2)).setText(R.string.empty);
+				((TextView)findViewById(R.id.textView3)).setText(R.string.empty);
+				((TextView)findViewById(R.id.textView4)).setText(R.string.empty);
+				
+				((ToggleButton)findViewById(R.id.toggleButton1)).setEnabled(false);
+				((ToggleButton)findViewById(R.id.toggleButton2)).setEnabled(false);
+				((ToggleButton)findViewById(R.id.toggleButton3)).setEnabled(false);
+				((ToggleButton)findViewById(R.id.toggleButton4)).setEnabled(false);
+			}
+		});
+    }
+    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menuAbout:
+			startActivityForResult(new Intent(Generate.this, About.class), 0);
+			return true;
+		case R.id.menuXKCD:
+			Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://xkcd.com/936/"));
+			startActivity(browserIntent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+    }
+    
+    private void readWordlist() {
+    	this.wordList = new ArrayList<String>();
+    	BufferedReader br = null;
+    	try {
+    		br = new BufferedReader(new InputStreamReader(getAssets().open("words")));
+    		String word;
+    		while((word=br.readLine()) != null)
+    			this.wordList.add(word);
+    	} catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    	finally {
+    		try {
+    			br.close(); //stop reading
+    		}
+    		catch(IOException ex) {
+    			ex.printStackTrace();
+    		}
+    	}
+    }
+    
+    /** generate a random common word **/
+    public String getWord() {
+    	SecureRandom sr = new SecureRandom();
+    	int index = sr.nextInt(wordList.size());
+    	return this.wordList.get(index);
+    }
+    
+    
+}
